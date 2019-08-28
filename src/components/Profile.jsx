53f8044629fb31211a1c25ca641eb58a28bd4a62
Anton <AntonKosmos@gmail.com>
2019-08-28 18:28:04 +0300

    make components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className = "content"> 
+      <div>
+        <img src ="https://thypix.com/wp-content/uploads/2018/05/Sommerlandschaft-Bilder-99.jpg"></img>
+      </div>
+      <div>
+        ava+discription
+      </div>
+      <div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            post1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+
+};
+
+export default Profile;
+
