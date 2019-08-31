3038d973d0159fa909cb1a9c337b82d8fbea9ce6
Anton <AntonKosmos@gmail.com>
2019-08-31 18:08:35 +0300

    create folder Dialogs with Dialogs.jsx and Dialogs.module.css

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+const Dialogs = () => {
+    return (
+        <div className = {s.item}>
+            Dialogs
+        </div>
+    );
+};
+
+export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,18 +3,19 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post.jsx'
 
 const MyPosts = () => {
-    return (
-      <div>
-        <div className = {s.item}>
-          my posts
+  return (
+    <div>
+      <div className={s.item}>
+        my posts
           <div>
-            <textarea></textarea>
-            <button>Add</button>
-          </div>
-          <Post/>
+          <textarea></textarea>
+          <button>Add</button>
         </div>
+        <Post message = "Hello, I learn React!" counts = "20"/>
+        <Post message = "Hi, and i also" counts = "27"/>
       </div>
-    );
+    </div>
+  );
 
 };
 
