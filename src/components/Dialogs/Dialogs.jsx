4a4a7ea23b27a817df7b2d2aaaa06634a2e8fffa
Anton <AntonKosmos@gmail.com>
2019-09-01 15:10:30 +0300

    work with Dialogs.jsx: create block dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import s from './Dialogs.module.css';
 const Dialogs = () => {
     return (
-        <div className = {s.item}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItem}>
+                <div className={s.dialog}>
+                    Anton
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Yra
+                </div>
+                <div className={s.dialog}>
+                    Sergey
+                </div>
+            </div>
         </div>
     );
 };
