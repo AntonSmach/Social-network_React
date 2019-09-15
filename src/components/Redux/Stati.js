016b2db7a8b2a04251ef4b43372d94a751f636e0
Anton <AntonKosmos@gmail.com>
2019-09-15 21:13:32 +0300

    creat folder Redux > state.js

---
+++ src/components/Redux/Stati.js
@@ -0,0 +1,17 @@
+let state = {
+    dialogsData: [
+        { id: "1", name: "Anton", },
+        { id: "2", name: "Sasha", },
+        { id: "3", name: "Valera", },
+    ],
+    postData: [
+        { message: "Hello, I learn React!", countsLike: "27" },
+        { message: "Hi, and i also", countsLike: "20" },
+    ],
+    messagesData: [
+        { message: "Hi" },
+        { message: "Yo" },
+    ]
+};
+
+export default state;
